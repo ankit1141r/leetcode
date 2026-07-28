e160a57ad704705ce7c3351cde487ffb54fed232
Ankit Rajput <ak7047053707@gmail.com>
2026-07-28 18:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.29%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
        
-         prev,maximum = 0,0
-         for num in nums:
-            temp=max(maximum,prev+num)
-            prev,maximum=maximum,temp
-        
-         return maximum
+        memo = {}
+
+        def f(i):
+            if i < 0:
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i]=max(f(i-1),nums[i]+f(i-2))
+            return memo[i]
+
+        return f(len(nums)-1)
 
 
