0f2e72091086d515b543246e41f3230c3bbf7dfa
Ankit Rajput <ak7047053707@gmail.com>
2026-09-23 20:12:48 +0530

    Time: 11 ms (52.14%) | Memory: 19.3 MB (81.45%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1:
+            return s
+        matrix =  [""]*numRows
+        currRow = 0
+        dir = -1
+        for ch in s:
+            matrix[currRow] += ch
+            if currRow == 0 or currRow== numRows-1:
+                dir*=-1
+            
+            currRow +=dir
+        
+        return "".join(matrix)
