8ea7dcb439fcc0a6d6aa79b8127565c113602820
Ankit Rajput <ak7047053707@gmail.com>
2026-09-25 23:21:01 +0530

    Time: 1330 ms (81.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,24 @@
+SELECT results
+FROM (
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr
+      ON u.user_id = mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(mr.movie_id) DESC, u.name
+    LIMIT 1
+) AS top_user
+
+UNION ALL
+
+SELECT results
+FROM (
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
+      ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, m.title
+    LIMIT 1
+) AS top_movie;
